9b15a74c8b840cdd4a4762c6c8e1134a9ee38fd9
Abhishek Chauhan <abhishekchauhan7898992278@gmail.com>
2026-02-16 16:34:22 +0530

    c

--- src/env.ts
+++ src/env.ts
@@ -13,13 +13,26 @@
 // }
  
 
+// export const sqlConnection = {
+//     user: 'sle',
+//     password: 'ciplsurvey@2019',
+//     server: '40.127.190.1',
+//     port: 1433,
+//     database: 'EdtsDubai2024',
+//     options: {
+//         encrypt: false, // Enable this if using Azure SQL Database
+//     },
+// }
+
 export const sqlConnection = {
-    user: 'sle',
-    password: 'ciplsurvey@2019',
-    server: '40.127.190.1',
-    port: 1433,
-    database: 'EdtsDubai2024',
-    options: {
-        encrypt: false, // Enable this if using Azure SQL Database
-    },
-}
+  user: 'sle',
+  password: 'ciplsurvey@2019',
+  server: '40.127.190.1',
+  port: 1433,
+  database: 'EdtsDubai2024',
+  options: {
+    encrypt: true, // Azure के लिए true
+    trustServerCertificate: true, // ✅ Self-signed certificate trust करने के लिए
+    connectionTimeout: 3600000,
+  },
+};
